bc8a06a3c9a8d94f6911e269bd72291053adc5be
dev-athome <l_sh@athomecorp.com>
2023-05-17 20:50:47 +0900

    card list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import './App.css';
+import { useState } from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [shoes] = useState(data);
+
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Game Shop</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            <div className="main-bg"></div>
+            <Container fluid="md">
+                <Row>
+                    <Col>
+                        <img
+                            src="https://codingapple1.github.io/shop/shoes1.jpg"
+                            width="80%"
+                            alt=""
+                        />
+                        <h4>{shoes[0].title}</h4>
+                        <p>{shoes[0].content}</p>
+                    </Col>
+                    <Col>
+                        <img
+                            src="https://codingapple1.github.io/shop/shoes2.jpg"
+                            width="80%"
+                            alt=""
+                        />
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                    <Col>
+                        <img
+                            src="https://codingapple1.github.io/shop/shoes3.jpg"
+                            width="80%"
+                            alt=""
+                        />
+                        <h4>상품명</h4>
+                        <p>상품설명</p>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
