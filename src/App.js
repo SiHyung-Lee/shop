7b1dd1e1616ff7a63950e1cf6bf61b97ace186d3
dev-athome <l_sh@athomecorp.com>
2023-05-30 17:11:18 +0900

    detail

--- src/App.js
+++ src/App.js
@@ -7,35 +7,57 @@ import Col from 'react-bootstrap/Col';
 import './App.css';
 import { useState } from 'react';
 import data from './data';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Detail from './routes/detail';
+import Event from './routes/event';
 
 function App() {
     let [shoes] = useState(data);
+    const navigate = useNavigate();
 
     return (
         <div className="App">
-            <Routes>
-                <Route path="/" element={<div>Main page</div>} />
-                <Route path="/detail" element={<div>Detail page</div>} />
-            </Routes>
-
             <Navbar bg="dark" variant="dark">
                 <Container>
                     <Navbar.Brand href="#home">Shoes Shop</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Cart</Nav.Link>
+                        <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
+                        <Nav.Link onClick={() => navigate('/detail')}>
+                            Detail
+                        </Nav.Link>
+                        <Nav.Link onClick={() => navigate('/event')}>
+                            Event
+                        </Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
-            <div className="main-bg"></div>
-            <Container fluid="md">
-                <Row>
-                    {shoes.map((shoe) => {
-                        return <Card shoe={shoe} />;
-                    })}
-                </Row>
-            </Container>
+
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <>
+                            <div className="main-bg"></div>
+                            <Container fluid="md">
+                                <Row>
+                                    {shoes.map((shoe) => {
+                                        return <Card shoe={shoe} />;
+                                    })}
+                                </Row>
+                            </Container>
+                        </>
+                    }
+                />
+                <Route path="/detail" element={<Detail />} />
+                <Route path="/event" element={<Event />}>
+                    <Route
+                        path="one"
+                        element={<p>첫 주문시 양배추즙 서비스</p>}
+                    />
+                    <Route path="two" element={<p>생일기념 쿠폰받기</p>} />
+                </Route>
+                <Route path="*" element={<div>No page</div>} />
+            </Routes>
         </div>
     );
 }
